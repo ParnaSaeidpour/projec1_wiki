a666ffbb34b1d1972fe2c6527be6e2a35937c0e6
Parna Saeidpour <parnasaeidpour@gmail.com>
2021-10-05 15:27:16 +0200

    Add Code

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("wiki/<str:page>", views.Entry_Page, name="Entry_Page")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 from . import util
+
+import markdown2
+from markdown2 import Markdown
+from django import forms
 
 
 def index(request):
@@ -8,3 +13,17 @@ def index(request):
         "entries": util.list_entries()
     })
 
+
+def Entry_Page(request, page):
+    try:
+
+        entry = util.get_entry(page)
+        markdowner = Markdown()
+        contents = markdowner.convert(entry)
+
+        return render(request, "encyclopedia/EntryPage.html", {
+            "title": page,
+            "content": contents
+        })
+    except TypeError:
+        return render(request, "encyclopedia/errorpage.html")
