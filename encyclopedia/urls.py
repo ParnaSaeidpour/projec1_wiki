387d73dc94349eaeb6476adfb49ea01656f91cb5
Parna Saeidpour <parnasaeidpour@gmail.com>
2021-10-13 20:10:48 +0200

    add

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("wiki/<str:page>", views.Entry_Page, name="Entry_Page")
+    path("wiki/<str:page>", views.Entry_Page, name="Entry_Page"),
+    path("Search_Page", views.Search_Page, name="Search_Page")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 from . import util
 
@@ -22,8 +23,25 @@ def Entry_Page(request, page):
         contents = markdowner.convert(entry)
 
         return render(request, "encyclopedia/EntryPage.html", {
-            "title": page,
+            "title": page,  
             "content": contents
         })
     except TypeError:
         return render(request, "encyclopedia/errorpage.html")
+
+
+def Search_Page(request):
+
+    results = []
+    query_searched = request.POST['q']
+    entries = util.list_entries()
+
+    for entry in entries:
+        if(entry.find(query_searched)==0):
+            pass
+        else:
+            results.append(entry)
+
+    return render(request,"encyclopedia/searchpage.html", {
+        "results" : results
+    })
